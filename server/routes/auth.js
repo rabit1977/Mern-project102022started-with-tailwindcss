c2e7edda921dde1b021b658cdd769ef2d9b8099b
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2022-10-27 23:52:12 +0200

    commited

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,6 +10,9 @@ import { register, login, secret } from '../controllers/auth.js';
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/auth-check', requireSignin, (req, res) => {
+  res.json({ ok: true });
+});
 
 // testing
 router.get('/secret', requireSignin, isAdmin, secret);
